0d32f9f6155b5d773f0a8c26425d5aef86e05de8
skaran921 <skaran921@gmail.com>
2020-08-05 07:03:03 +0530

    #authTokenDone

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Login from "./auth/Login";
+import { getAuthTokenFromSpotifyResponse } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const spotify = new SpotifyWebApi();
+const App = () => {
+  const [token, setToken] = useState(null);
+  // runCode
+  useEffect(() => {
+    const hash = getAuthTokenFromSpotifyResponse();
+    const _token = hash.access_token;
+    window.location.hash = "";
+    if (_token) {
+      setToken(_token);
+    }
+    console.log("token", _token);
+    spotify.setAccessToken(_token);
+    spotify
+      .getMe()
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return <div>{token ? <h1>i am logged in</h1> : <Login />}</div>;
+};
 
 export default App;
