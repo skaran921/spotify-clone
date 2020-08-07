fe9759fe459bff89a7a0fc52305768503d4df809
skaran921 <skaran921@gmail.com>
2020-08-07 17:36:13 +0530

    #body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "../styles/body.css";
 import Header from "./Header";
-
+import logo from "../logos/logo7.png";
 const Body = ({ spotify }) => {
   return (
     <div className="body">
       <Header spotify={spotify} />
+      <div className="body-info">
+        <img src={logo} alt="" />
+        <div className="body-info-Text">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>Description</p>
+        </div>
+      </div>
     </div>
   );
 };
