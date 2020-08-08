35b217a55f22225edf38c41d352cc126c01e11a7
skaran921 <skaran921@gmail.com>
2020-08-08 21:26:14 +0530

    #done

--- src/context/actions.js
+++ src/context/actions.js
@@ -1,3 +1,4 @@
 export const SET_USER_ACTION = "SET_USER";
 export const SET_TOKEN_ACTION = "SET_TOKEN";
 export const SET_PLAYLIST_ACTION = "SET_PLAYLIST";
+export const SET_BOLLYWOOD_ACTION = "SET_BOLLYWOOD";
